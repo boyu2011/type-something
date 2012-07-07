5033822742cebe5f5a9ed35443fef9c9692ddf0d
boyu2011 <boyu2011@gmail.com>
2012-07-07 01:02:47 -0400

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ end
 
 group :test do
 	gem 'factory_girl_rails', '1.4.0'
+	gem 'cucumber-rails', '1.2.1', require: false
+	gem 'database_cleaner', '0.7.0'
 end
 
 group :prodction do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,18 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-  	def new
-  		@user = User.new
-  	end
+    def new
+        @user = User.new
+    end
 
-  	def create
-  		@user = User.new(params[:user])
-  		if @user.save
-  			flash[:success] = "Welcome to the Type Something App!"
-  			redirect_to @user
-  		else
-  			render 'new'
-  		end
-  	end
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+            sign_in @user
+            flash[:success] = "Welcome to the Type Something App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
 end
